f95edc9d61dff41f4072c84d0200175738c87969
zhouhui0802 <1741484908@qq.com>
2024-11-28 12:10:50 +0800

    工具类进场

--- src/main/java/com/zh/util/JwHelper.java
+++ src/main/java/com/zh/util/JwHelper.java
@@ -1,4 +1,65 @@
 package com.zh.util;
 
+import io.jsonwebtoken.*;
+import org.springframework.util.StringUtils;
+
+import java.util.Date;
+
 public class JwHelper {
+
+    private static long tokenExpiration = 24*60*60*1000;
+    private static String tokenSignKey = "123456";
+
+    //生成token字符串
+    public static String createToken(Long userId, Integer userType) {
+        String token = Jwts.builder()
+
+                .setSubject("YYGH-USER")
+
+                .setExpiration(new Date(System.currentTimeMillis() + tokenExpiration))
+
+                .claim("userId", userId)
+
+                .claim("userType", userType)
+
+                .signWith(SignatureAlgorithm.HS512, tokenSignKey)
+                .compressWith(CompressionCodecs.GZIP)
+                .compact();
+        return token;
+    }
+
+    //从token字符串获取userid
+    public static Long getUserId(String token) {
+        if(StringUtils.isEmpty(token)) return null;
+        Jws<Claims> claimsJws = Jwts.parser().setSigningKey(tokenSignKey).parseClaimsJws(token);
+        Claims claims = claimsJws.getBody();
+        Integer userId = (Integer)claims.get("userId");
+        return userId.longValue();
+    }
+
+    //从token字符串获取userType
+    public static Integer getUserType(String token) {
+        if(StringUtils.isEmpty(token)) return null;
+        Jws<Claims> claimsJws
+                = Jwts.parser().setSigningKey(tokenSignKey).parseClaimsJws(token);
+        Claims claims = claimsJws.getBody();
+        return (Integer)(claims.get("userType"));
+    }
+
+    //判断token是否有效
+    public static boolean isExpiration(String token){
+        try {
+            boolean isExpire = Jwts.parser()
+                    .setSigningKey(tokenSignKey)
+                    .parseClaimsJws(token)
+                    .getBody()
+                    .getExpiration().before(new Date());
+            //没有过期，有效，返回false
+            return isExpire;
+        }catch(Exception e) {
+            //过期出现异常，返回true
+            return true;
+        }
+    }
+
 }
